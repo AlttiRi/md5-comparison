c6ff247b6cbdbf80e351b7e1ed961d5e77ab3819
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2020-08-24 17:18:53 +0300

    Add the script for the libraries bundling

---
+++ building/bundle-libs.js
@@ -0,0 +1,70 @@
+import {build, minify, sourceMappingURL, write} from "./common.js";
+import {appendFinally} from "./rollup-plugins.js";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+
+import browserify from "browserify";
+
+
+
+function getInputOptions(input, filename) {
+    return {
+        input,
+        plugins: [
+            commonjs(),
+            resolve({
+                browser: true,
+            }),
+            appendFinally(sourceMappingURL(filename))
+        ]
+    }
+}
+
+/** @type {import("rollup").OutputOptions} */
+const outputOptions = {
+    format: "iife",
+    sourcemap: true
+};
+
+!async function main() {
+    const promises = [];
+
+    const dist = "../dist/vendor/";
+    const hashers = ["js-md5", "spark-md5", "blueimp-md5", "cryptojs-md5"];
+
+    for (const hasher of hashers) {
+        const input = `../vendor/exposes/${hasher}.es.js`;
+        const filename = `${hasher}.rolluped`;
+
+        console.log(hasher);
+        const done =  await build(getInputOptions(input, filename), outputOptions, filename, dist);
+        promises.push(done);
+    }
+
+
+    const hashers2 = ["cb-md5", "node-md5"];
+
+    for (const hasher of hashers2) {
+        const input = `../vendor/exposes/${hasher}.cjs.js`;
+        const filename = `${hasher}.browserified`;
+
+        console.log(hasher);
+        const b = browserify(input,{debug: true});
+        const output = await new Promise(resolve => b.bundle((error, body) => resolve(body.toString())));
+
+        const sourceMappingString = "//# sourceMappingURL=data:application/json;charset=utf-8;base64,"
+        const offset = output.lastIndexOf(sourceMappingString);
+        const mapBase64 = output.substring(offset + sourceMappingString.length);
+
+        const map = JSON.parse(Buffer.from(mapBase64, "base64").toString("binary"));
+        const code = output.substring(0, offset) + sourceMappingURL(filename);
+
+        const written = write(code, map, filename + ".js", dist);
+
+        const {code: codeMin, map: mapMin} = await minify(code, map, filename);
+        const writtenMin = write(codeMin, mapMin, filename + ".min.js", dist);
+
+        promises.push(written, writtenMin);
+    }
+    await Promise.all(promises);
+}();
